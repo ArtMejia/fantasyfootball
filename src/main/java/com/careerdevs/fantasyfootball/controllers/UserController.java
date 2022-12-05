d2b6fc9372bb445345a5de60abe922a26bc23227
ArtMejia <arteniomejia.student@careerdevs.com>
2022-12-05 17:54:37 -0500

    Add player routes createAPlayer, getAllPlayers, getPlayerById, and deletePlayerById

--- src/main/java/com/careerdevs/fantasyfootball/controllers/UserController.java
+++ src/main/java/com/careerdevs/fantasyfootball/controllers/UserController.java
@@ -1,4 +1,11 @@
 package com.careerdevs.fantasyfootball.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 }
